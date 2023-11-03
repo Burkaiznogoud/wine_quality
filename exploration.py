0b71074f507970abde2b1edeb59a6ce4d8602def
Michal Jedzierowski <micjedzierowski.com>
2023-11-03 11:08:55 +0100

    Added function to list features for further investigation

--- exploration.py
+++ exploration.py
@@ -6,9 +6,6 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_colwidth', None)
 wine = pd.read_csv("RedWine.csv")
 
-print(wine["Ph"])
-print(wine[["Volatile acidity"]])
-
 def create_scatter_plot(data, x_factor, y_factor):
     plt.figure(figsize = (12 , 6))
     plt.scatter(data[x_factor], data[y_factor], marker='.')
@@ -17,6 +14,22 @@ def create_scatter_plot(data, x_factor, y_factor):
     plt.title(f"{y_factor} on {x_factor}", fontsize = 16, fontweight = 'bold')
     plt.show()
 
-create_scatter_plot(data = wine , x_factor = "Ph", y_factor= "Volatile acidity")
+def column_names(dataframe):
+    col_names = {number : col_name for number, col_name in enumerate(dataframe.columns)}
+    return col_names
 
-
+def list_options(data, plot):
+    col_names = column_names(data)
+    for key, value in col_names.items():
+        print(key, value)
+    try:
+        first = int(input("Choose first factor by number: "))
+        second = int(input("Choose second factor by number: "))
+        plot(data, col_names[first], col_names[second])
+    except ValueError:
+        print("Value error! Choose number from list.")
+    else:
+        return
+    
+list_options(data=wine, plot=create_scatter_plot)
+    
