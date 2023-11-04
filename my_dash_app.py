4617ce13bf041c18edf78c0498f8ada127e2c768
Michal Jedzierowski <micjedzierowski.com>
2023-11-04 13:09:47 +0100

    updated my_dash_app.py to show first graph

--- my_dash_app.py
+++ my_dash_app.py
@@ -4,6 +4,7 @@ import dash
 import dash_html_components as html
 import dash_core_components as dcc
 from dash.dependencies import Input, Output
+from exploration import create_scatter_plot
 import plotly.express as px
 
 file = 'RedWine.csv'
@@ -27,21 +28,29 @@ app.layout = html.Div(children = [html.H1("Wine Quality Dashboard",
                                                    'font-size': 40}),
                                   html.Br(),
                                   html.Div(
-                                      dcc.Dropdown(id = 'drop_x', options = labels, placeholder = 'X axis', searchable = True)),
+                                      dcc.Dropdown(id = 'drop_x', options = labels, placeholder = 'X axis', searchable = True, value=labels[0]['value'])),
                                   html.Div(
-                                      dcc.Dropdown(id = 'drop_y', options = labels, placeholder = 'Y axis', searchable = True)),
+                                      dcc.Dropdown(id = 'drop_y', options = labels, placeholder = 'Y axis', searchable = True, value=labels[1]['value'])),
                                   html.Br(),
                                   html.Div(
                                       dcc.Graph(id = 'top_graph')),
                                   html.Br(),
                                   html.Div(
-                                      dcc.RangeSlider(id = 'slider_x')),
+                                      dcc.RangeSlider(id = 'slider_horizontal')),
                                   html.Br(),
                                   html.Div(
                                       dcc.Graph(id = 'bottom_graph'))
                                 ]
                         )
-
+@app.callback(
+        Output(component_id = 'top_graph', component_property = 'figure'),
+        [Input(component_id = 'drop_x', component_property = 'value'),
+         Input(component_id = 'drop_y', component_property = 'value')],
+)
+def get_scatter_chart(entered_value_x, entered_value_y):
+    df_entered = df
+    fig = px.scatter(df_entered, x = entered_value_x, y = entered_value_y)
+    return fig
 
 
 if __name__ == '__main__':
