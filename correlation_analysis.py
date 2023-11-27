b4041ccd0419d33cf04a971fe7e464961df31989
Michal Jedzierowski <micjedzierowski.com>
2023-11-27 14:49:55 +0100

    Added plots directory and generated meshgrid_features.png and cm_features.png

--- correlation_analysis.py
+++ correlation_analysis.py
@@ -1,28 +1,38 @@
-from prepare_file import PrepareFile, Data
+import prepare_file as f
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from seaborn.objects import Plot as plot
+from seaborn.objects import Dots as dot
 import matplotlib.pyplot as plt
 from sklearn.linear_model import RidgeClassifier
 from sklearn.metrics import accuracy_score, classification_report, accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 from misc.evaluation import evaluation
 
-
-
-data = PrepareFile(file='.\datafiles\Redwine.csv', Y_column='Recommended', columns_to_exclude=['Quality', 'Ph', 'Free sulfur dioxide', 'Density', 'Fixed acidity', 'Volatile acidity'])
+data = f.PrepareFile(file='.\datafiles\Redwine.csv', Y_column='Recommended', columns_to_exclude=['Quality', 'Ph', 'Free sulfur dioxide', 'Density', 'Fixed acidity', 'Volatile acidity'])
 
 correlation_matrix = pd.DataFrame(data.X).corr()
 
-plt.figure(figsize=(10, 7))
-sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f", xticklabels=data.X_columns, yticklabels=data.X_columns)
-plt.show()
+# plt.figure(figsize=(10, 7))
+# sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f", xticklabels=data.X_columns, yticklabels=data.X_columns)
+# plt.show()
 
-d = Data()
+d = f.Data()
 
 X_train, X_test, Y_train, Y_test, columns = d.data
 ridge = RidgeClassifier()
 ridge.fit(X_train, Y_train)
 y_hat = ridge.predict(X_test)
+
+def plot_meshgrid():
+    raw_data = d.get_raw()
+    features = pd.DataFrame(raw_data['X_raw'])
+    pairplot = sns.pairplot(features, kind='scatter', diag_kind='kde', height=1, aspect=1)
+    for axes in pairplot.axes.flat:
+        axes.set_ylabel(axes.get_ylabel(), rotation=60)
+        axes.set_xlabel(axes.get_xlabel(), rotation=45)
+    plt.savefig(fname="plots\meshgrid_feature.png")
+    plt.close()
 
 @evaluation
 def results():
@@ -45,7 +55,9 @@ def plot_confusion_matrix():
     ax.set_title('Confusion Matrix'); 
     ax.xaxis.set_ticklabels(['Not recommended', 'Recommended']) 
     ax.yaxis.set_ticklabels(['Not recommended', 'Recommended'])
-    plt.show()
+    plt.savefig(fname='plots\cm_features.png')
+    plt.close()
         
 results()
+plot_meshgrid()
 plot_confusion_matrix()
--- prepare_file.py
+++ prepare_file.py
@@ -3,6 +3,8 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 
+_ = 20 * '-'
+
 class PrepareFile:
     def __init__(self, file, Y_column, columns_to_exclude=None):
         self.exclude = columns_to_exclude
@@ -13,52 +15,60 @@ class PrepareFile:
         self.X_columns = [col for col in self.file.columns if col != self.Y_column and col not in self.exclude]
 
     def options(self, file):
-        print(20 * "-")
-        print(f"Reading {file} file from CSV.")
-        print(20 * "-")
+        text = f"{_}\nReading {file} file from CSV..\n{_}"
+        print(text)
         self.file = pd.read_csv(file)
         pd.set_option('display.max_columns', None)
         pd.set_option('display.max_colwidth', None)
         return self.file
     
     def prepare_Y(self, Y_column):
-        print(20 * "-")
-        print(f"Extracting {Y_column} and converting it to numpy.")
-        print(20 * "-")
+        text = f"{_}\nExtracting {Y_column} and converting it to numpy.\n{_}"
+        print(text)
         Y = self.file[Y_column]
         Y = Y.replace({True: np.float64(1), False: np.float64(0)})
+        self.Y_raw = Y
         self.Y = Y.to_numpy()
         self.file.drop(Y_column, axis=1, inplace=True)
         self.file.set_index('Id', inplace=True)
-        return self.Y
+        return self.Y, self.Y_raw
 
     def prepare_X(self, columns_to_exclude=None):
         if columns_to_exclude == None:
-            print(20 * "-")
-            print(f"No columns were excluded. Left dataframe is being converted to numpy array.")
-            print(20 * "-")
+            text = f"{_}\nNo columns were excluded. Left dataframe is being converted to numpy array.\n{_}"
+            print(text)
             X = self.file
+            self.X_raw = X
             self.X = X.to_numpy()
         else:
-            print(20 * "-")
-            print(f"Columns {columns_to_exclude} were excluded. Left dataframe is being converted to numpy array.")
-            print(20 * "-")
+            text = f"{_}\nColumns {columns_to_exclude} were excluded. Left dataframe is being converted to numpy array.\n{_}"
+            print(text)
             X = self.file.drop(columns_to_exclude, axis=1).reset_index(drop=True)
+            self.X_raw = X
             self.X = X.to_numpy()
-        return self.X
+        return self.X, self.X_raw
     
 
 class Data:
     def __init__(self):
+        self.extract_values()
         self.prepare_data()
 
-    def prepare_data(self):
+    def extract_values(self):
         data = PrepareFile(file='.\datafiles\Redwine.csv', Y_column='Recommended', columns_to_exclude=['Quality'])
-        Y = data.Y
-        X = data.X
-        columns = data.X_columns
+        self.Y = data.Y
+        self.X = data.X
+        self.Y_raw = data.Y_raw
+        self.X_raw = data.X_raw
+        self.columns = data.X_columns
+        return self.Y, self.X, self.Y_raw, self.X_raw, self.columns
+
+    def get_raw(self):
+        return {'Y_raw': self.Y_raw, 'X_raw': self.X_raw}
+
+    def prepare_data(self):
         transform = StandardScaler()
-        X = transform.fit_transform(data.X)  
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=4)
-        self.data = X_train, X_test, Y_train, Y_test, columns
+        X = transform.fit_transform(self.X)  
+        X_train, X_test, Y_train, Y_test = train_test_split(X, self.Y, test_size=0.2, random_state=4)
+        self.data = X_train, X_test, Y_train, Y_test, self.columns
         return self.data
